440d54c2cf7f9e1e1c72b37c8ec17856d0a9f79f
Gui-lira <gui.lira.martins@gmail.com>
2021-02-25 16:20:50 -0300

    bonus 1

---
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_2/bonus.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+for (let x = 0;x < numbers.length;x++) 
+    {for (let i = 1;i < numbers.length;i++)
+        {for (let j = 0;j < i;j++)
+            {if (numbers[j] > numbers[i])
+                {let posicao = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = posicao}}};}
+
+console.log(numbers)
--- bloco_4/introducao-a-javascript-e-logica-de-programacao_2/exer2.js
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_2/exer2.js
@@ -3,4 +3,5 @@ let ate25 = [];
 for (let contador = 1;contador <= 25;contador++)
     {ate25.push(contador)};
 
-console.log(ate25)
+for (let indice = 0;indice < ate25.length;indice++)
+    {console.log(ate25[indice]/2)}
