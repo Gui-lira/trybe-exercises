d14abfcbfa58242ecb24ad245c8a5f3c33e7379e
Gui-lira <gui.lira.martins@gmail.com>
2021-02-25 15:25:35 -0300

    exercicios ate o o 4

---
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_2/exer.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+
+for (let indice = 0;indice < numbers.length;indice++)
+    {soma = soma + numbers[indice]};
+
+if (soma/numbers.length > 20) 
+    {console.log('valor maior que 20')}
+
+else {console.log('valor menor ou igual a 20')}
