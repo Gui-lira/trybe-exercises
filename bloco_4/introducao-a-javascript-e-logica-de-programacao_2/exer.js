7246e3ca8fccc886cf19fb1a70d54f554b8b2a79
Gui-lira <gui.lira.martins@gmail.com>
2021-02-25 15:31:01 -0300

    exerciciio 7

--- bloco_4/introducao-a-javascript-e-logica-de-programacao_2/exer.js
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_2/exer.js
@@ -1,9 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let maior = numbers[0];
+let menor = numbers[0];
 
 for (let indice = 0;indice < numbers.length;indice++)
-    {if (maior < numbers[indice])
-        {maior = numbers[indice]}};
+    {if (menor > numbers[indice])
+        {menor = numbers[indice]}};
 
-console.log(maior)
+console.log(menor)
