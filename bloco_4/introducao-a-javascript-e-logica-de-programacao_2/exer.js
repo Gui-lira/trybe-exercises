cdce1d5e22b319a039c6e4d9ac577956dbf8664d
Gui-lira <gui.lira.martins@gmail.com>
2021-02-25 15:33:48 -0300

    exercicio 6

--- bloco_4/introducao-a-javascript-e-logica-de-programacao_2/exer.js
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_2/exer.js
@@ -1,9 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let menor = numbers[0];
+let quantImpar = 0;
 
 for (let indice = 0;indice < numbers.length;indice++)
-    {if (menor > numbers[indice])
-        {menor = numbers[indice]}};
+    {if (numbers[indice] % 2 != 0)
+        {quantImpar += 1}};
 
-console.log(menor)
+console.log(quantImpar)
