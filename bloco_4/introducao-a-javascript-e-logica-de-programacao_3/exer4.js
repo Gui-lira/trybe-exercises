a830694ef7597445bb37027b22e42c96283d3702
Gui-lira <gui.lira.martins@gmail.com>
2021-02-26 16:10:47 -0300

    exercicio 4

---
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_3/exer4.js
@@ -0,0 +1,6 @@
+let n = 5;
+let asterisco = '*';
+let space = ' '
+let floors = 1
+for (let times = 1;times <= 5;times += 2)
+    {console.log(space.repeat((Math.ceil(n-times))/2) + asterisco.repeat(times))}
