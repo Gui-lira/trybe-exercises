8f20f460bb421b97b01d70068ba2863bdff122f4
Gui-lira <gui.lira.martins@gmail.com>
2021-02-26 15:46:30 -0300

    exercicio 3

--- bloco_4/introducao-a-javascript-e-logica-de-programacao_3/exer2.js
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_3/exer2.js
@@ -2,5 +2,5 @@ let asterisco = '*';
 let n = 5;
 if (n < 1)
     {console.log('erro')}
-else {for (let bases = 1;bases <= 5;bases += 1)
+else {for (let bases = 1;bases <= n;bases += 1)
         {console.log(asterisco.repeat(bases))}}
---
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_3/exer3.js
@@ -0,0 +1,6 @@
+let space = ' '; 
+let asterisco = '*';
+let n = 5;
+
+for (let bases = 1;bases <= n;bases += 1)
+     {console.log(space.repeat(n-bases)+asterisco.repeat(bases))}
