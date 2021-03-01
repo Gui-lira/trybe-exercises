38f6ef1ff037275f4e70181e3d435090156b3be3
Gui-lira <gui.lira.martins@gmail.com>
2021-03-01 16:26:39 -0300

    exercicio 3.2

--- bloco_4/introducao-a-javascript-e-logica-de-programacao_4/exer2.js
+++ bloco_4/introducao-a-javascript-e-logica-de-programacao_4/exer2.js
@@ -7,3 +7,10 @@ function palindrome(str) {
             }
         }
 };
+function minor(numbers) {let minor = numbers[0];
+                        for (let index in numbers)
+                                {if (numbers[index] < minor)
+                                    {minor = numbers[index]}
+                                };
+                        return minor; };
+console.log(minor([2, 4, 6, 7, 10, 0, -3]))
