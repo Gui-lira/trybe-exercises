c24bc4412b3576be351b3625ee31e6fc4ed53d81
Gui-lira <gui.lira.martins@gmail.com>
2021-04-13 18:15:18 -0300

    bonus

--- bloco_9/javascript-assincrono-e-promises.2/2.js
+++ bloco_9/javascript-assincrono-e-promises.2/2.js
@@ -1,19 +1,29 @@
-
-const tryPromise = () => {
-const promise = new Promise((resolve, reject) => {
+const promise = () => {
     const list = [];
     for (let timer = 0; timer < 10; timer += 1) {
         const number = Math.round(Math.random() * 50) + 1;
         list.push(number);
     };
     const sum = list.map((item) => item * item).reduce((acumulator, item) => acumulator += item);
-    (sum > 8000) ? resolve(sum) : reject();    
-});
+    if (sum < 8000) return sum
+     throw new Error();    
+};
 
+const SumReduce = (valor) => {
+    let list =  [1,2,3,5,10];
+     return list.map((item) =>  valor / item)
+    .reduce((acumulator, item) => acumulator + item)
+}
 
-    promise
-    .then(sum => console.log([1,2,3,5,10].map((item) =>  sum / item)))
-    .catch(() => console.log('deu errado'))
+const tryPromise = async () => {
+   try {
+       const total = await promise();
+       const x = await SumReduce(total);
+       await console.log(x);
+   } 
+   catch(erro) {
+    console.log('É mais de oito mil! Essa promise deve estar quebrada!')
+   }
 }
 
 tryPromise();
