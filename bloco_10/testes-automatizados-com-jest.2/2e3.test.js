58091c027723135d9be13c186d6f398125d4878c
Gui-lira <gui.lira.martins@gmail.com>
2021-04-20 14:42:20 -0300

    3

--- bloco_10/testes-automatizados-com-jest.2/2e3.test.js
+++ bloco_10/testes-automatizados-com-jest.2/2e3.test.js
@@ -2,16 +2,29 @@ const { findUserById,
         getUserName } = require('./2e3');
 
 
-test('caso de sucesso', () => {
-    return getUserName(4)
-    .then((user) => {
-        expect(user).toBe('Mark' )
-    })
+// test('caso de sucesso', () => {
+//     return getUserName(4)
+//     .then((user) => {
+//         expect(user).toBe('Mark' )
+//     })
+// });
+
+// test('caso de falha', () => {
+//     return getUserName(13)
+//     .catch((error) => {
+//         expect(error).toEqual({ error: 'User with 13 not found.' })
+//     })
+// });
+
+test('sucesso usando await', async () => {
+    const userNAme = await getUserName(4);
+    expect(userNAme).toBe('Mark');
 });
 
-test('caso de falha', () => {
-    return getUserName(13)
-    .catch((error) => {
+test('falha usando await', async () => {
+    try {
+        await getUserName(13)
+    } catch (error) {
         expect(error).toEqual({ error: 'User with 13 not found.' })
-    })
-});
+    }
+})
