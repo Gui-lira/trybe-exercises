42954e8be8d7bc44a77a07d0565cb175267f90ef
Gui-lira <gui.lira.martins@gmail.com>
2021-04-22 16:05:01 -0300

    2

--- bloco_10/testes-automatizados-com-jest.3/1.test.js
+++ bloco_10/testes-automatizados-com-jest.3/1.test.js
@@ -9,4 +9,12 @@ test('exercicio 1', () => {
     expect(random.random100).toHaveBeenCalled()
     expect(random.random100()).toBe(10);
     expect(random.random100).toHaveBeenCalledTimes(2)
+});
+
+test('exercicio 2', () => {
+    random.random100 = jest
+    .fn()
+    .mockImplementation((a,b) => a / b);
+    expect(random.random100(2,1)).toBe(2);
+    expect(random.random100).toHaveBeenCalled();
 })
